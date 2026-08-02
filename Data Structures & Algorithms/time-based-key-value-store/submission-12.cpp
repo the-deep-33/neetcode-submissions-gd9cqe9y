e9deb0348da184d559d312ec0b64fe48e54f92cb
the-deep-33 <petnik033@gmail.com>
2026-08-02 13:36:50 +0200

    Add: time-based-key-value-store - submission-12

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-12.cpp
@@ -0,0 +1,43 @@
+class TimeMap {
+    unordered_map<string, map<int, string>> feeling_map;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        feeling_map[key].insert({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        int low = 0;
+        int high = feeling_map[key].size();
+
+        string candidate = "";
+
+        while(low < high)
+        {
+            cout << low << " " << high << endl;
+            int mid = low + (high-low) / 2;
+            auto it = feeling_map[key].begin();
+
+            advance(it, mid);
+            int time = it->first;
+
+            if(timestamp == time)
+            {
+                candidate = it->second;
+                return candidate;
+            }
+            else if(timestamp > it->first)
+            {
+                candidate = it->second;
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return candidate;
+    }
+};
